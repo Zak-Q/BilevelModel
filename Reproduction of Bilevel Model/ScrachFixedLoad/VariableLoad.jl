5bb7c8f152a90c05033868672d813efbc56f0086
Zihao_Qiu <qiuzihao200005@outlook.com>
2026-03-05 12:04:12 +1100

    single round running test and results

--- Reproduction of Bilevel Model/ScrachFixedLoad/VariableLoad.jl
+++ Reproduction of Bilevel Model/ScrachFixedLoad/VariableLoad.jl
@@ -1,8 +1,8 @@
 using Pkg
 
-pkg.add("Plots")
-pkg.add("BilevelJuMP")
-pkg.add("Gurobi")
+Pkg.add("Plots")
+Pkg.add("BilevelJuMP")
+Pkg.add("Gurobi")
 using Plots, BilevelJuMP, Gurobi
 
 #generators 
@@ -13,13 +13,14 @@ n_gens = length(Pmax)
 n_storages = 1 #socket for further developments
 
 # Variable demands
-bidding_demand = 150 # $/ MWh
-Pdemand = 200
-model = BilevelModel(solver=Gurobi.Optimizer,
-        mode = BilevelJuMP.FortunyAmatMcCarlMode(primal_big_M = 100, dual_big_M = 100))
+bidding_demand = [20, 30] # $/ MWh
+Pdemand = [120, 80]
+n_demands = length(Pdemand)
+model = BilevelModel(Gurobi.Optimizer,
+        mode = BilevelJuMP.FortunyAmatMcCarlMode(primal_big_M = 1000, dual_big_M = 1000))
 
 @variable(Lower(model), 0 <= pwr_gen[i=1:n_gens] <= Pmax[i])
-@variable(Lower(model), 0 <= pwr_load <= Pdemand)
+@variable(Lower(model), 0 <= pwr_load[i=1:n_demands] <= Pdemand[i])
 @variable(Lower(model), 0 <= pwr_charge)
 @variable(Lower(model), 0 <= pwr_discharge)
 
@@ -33,7 +34,48 @@ model = BilevelModel(solver=Gurobi.Optimizer,
 @variable(Upper(model), 0 <= SOC <= 50)
 
 
-@constraint(Lower(model), balance, sum(pwr_gen[i] for i in 1:n_gens) + pwr_discharge == pwr_load + pwr_charge)
+
+@constraint(Lower(model), balance, sum(pwr_gen[i] for i in 1:n_gens) + pwr_discharge == sum(pwr_load[i] for i in 1:n_demands) + pwr_charge)
 @constraint(Lower(model), charge_limit, pwr_charge <= pwr_charge_up)
 @constraint(Lower(model), discharge_limit, pwr_discharge <= pwr_discharge_up)
-@objective(Lower(model), Min, sum(price_discharge * pwr_discharge - price_charge * pwr_charge for i in 1:n_storages))
+
+@variable(Upper(model), Clearing_price, DualOf(balance))
+
+@objective(Lower(model), Min, sum(price_discharge * pwr_discharge - price_charge * pwr_charge for i in 1:n_storages)
+                              + sum(offer_price[i] * pwr_gen[i] for i in 1:n_gens)
+                              - sum(bidding_demand[i] * pwr_load[i] for i in 1:n_demands))
+
+
+@objective(Upper(model), Max, sum((Clearing_price + price_charge) * pwr_charge for i in 1:n_storages) 
+                              - sum((Clearing_price + price_discharge) * pwr_discharge for i in 1:n_storages))
+
+
+# solve the bilevel model
+optimize!(model)
+
+# check termination status before querying results
+println("Termination status: ", termination_status(model))
+
+if termination_status(model) == MOI.OPTIMAL || termination_status(model) == MOI.FEASIBLE
+    println("The optimal value of the upper-level problem is: ", objective_value(Upper(model)))
+    println("The optimal value of the lower-level problem is: ", objective_value(Lower(model)))
+    println("The optimal generation is: ", value.(pwr_gen))
+    println("The optimal load is: ", value.(pwr_load))
+    println("The optimal charging power is: ", value.(pwr_charge))
+    println("The optimal discharging power is: ", value.(pwr_discharge))
+    println("The optimal clearing price is: ", value.(Clearing_price))
+    println("The optimal price for charging is: ", value.(price_charge))
+    println("The optimal price for discharging is: ", value.(price_discharge))  
+else
+    println("No solution available (status: ", termination_status(model), ")")
+end
+
+# The optimal value of the upper-level problem is: 53000.0
+# The optimal value of the lower-level problem is: -50700.0
+# The optimal generation is: [40.0, 30.0, 60.0]
+# The optimal load is: [0.0, 80.0]
+# The optimal charging power is: 50.0
+# The optimal discharging power is: 0.0
+# The optimal clearing price is: 30.0
+# The optimal price for charging is: 1030.0
+# The optimal price for discharging is: 0.0
